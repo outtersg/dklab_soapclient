7024d3f19b3d4b2f20a7b4d3911a8f2a4b69ffb2
Guillaume Outters <guillaume-curl@outters.eu>
2022-09-24 22:44:24 +0200

    tests: server_launch.php: no diagnose
    bug: we are usually called during a run-tests.php, where our diags to STDERR are interpreted as deviant output from the tested program (thus making it considered as failed).

--- t/Dklab_SoapClient/fixture/server_launch.php
+++ t/Dklab_SoapClient/fixture/server_launch.php
@@ -2,6 +2,17 @@
 
 /*- Client-side --------------------------------------------------------------*/
 
+function serverLaunchLog($message)
+{
+	// @todo Make it opt-in
+	if ('run-tests.php does not like being polluted by STDERR diagnose') {
+		return;
+	}
+	$args = func_get_args();
+	array_unshift($args, STDERR);
+	call_user_func_array('fprintf', $args);
+}
+
 function serverRuns($host, $port, $concurrency = 1, $killIfNotEnoughConcurrency = false)
 {
 	$c = curl_init("http://$host:$port/server_launch.php");
@@ -24,7 +35,7 @@ function serverRuns($host, $port, $concurrency = 1, $killIfNotEnoughConcurrency 
 					$pid = $r->PPID;
 				}
 				// And whoops, it seems that it is not receptive anymore to SIGINT (only SIGTERM, which lets orphans to be killed individually).
-				fprintf(STDERR, "Killing server %d with concurrency %d...\n", $pid, $r->concurrency);
+				serverLaunchLog("Killing server %d with concurrency %d...\n", $pid, $r->concurrency);
 				system("kill $pid `ps -o pid,ppid | grep '[ 	]$pid$' | awk '{print\$1}'`");
 			}
 		}
@@ -45,7 +56,7 @@ function requireServer($host, $port, $concurrency = 1)
 	$env = $concurrency > 1 ? "PHP_CLI_SERVER_WORKERS=$concurrency " : '';
 	$here = dirname(__FILE__);
 	$cmd = "{$env}nohup php -S $host:$port -t $here";
-	fprintf(STDERR, "Starting %s...\n", strtr($cmd, array('nohup ' => '')));
+	serverLaunchLog("Starting %s...\n", strtr($cmd, array('nohup ' => '')));
 	system("cd /tmp/ && $cmd < /dev/null > /tmp/php$port.log 2>&1 &");
 	usleep(200000);
 
